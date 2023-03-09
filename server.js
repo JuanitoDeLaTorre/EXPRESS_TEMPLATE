f3d709733bd6793d7a9c173b1545ca1bbaf7d904
JuanitoDeLaTorre <home.john.thomas@gmail.com>
2023-03-08 19:40:52 -0500

    messed around with styling, axios API requests

--- server.js
+++ server.js
@@ -5,18 +5,50 @@ const app = express()
 const methodOverride = require("method-override")
 const path = require("path")
 const { Item } = require("./Models")
+const axios = require("axios")
 
 //Config
 const PORT = 3000
 
 //setting project parameters
 app.use(express.urlencoded({ extended: false }))
-
 app.set("views", path.join(__dirname,"views"))
 app.set("view engine","ejs")
 app.use(express.static("Public"))
 app.use(methodOverride('_method'))
 app.use(express.json())
+
+//function to get unsplash urls
+
+let unsplashResponse = []
+async function getPhotos(numPhotos) {
+    let imgURLs = []
+    
+    // try {
+    //    let respObject = await axios.get("https://api.unsplash.com/photos/random?count=15&query=abstract&client_id=2xkFYCZvt9fOK2jHkVG4pEayGyEOiwxdtOvPXwfeHPI").then((resp)=>{
+    //         for(let i = 0; i < resp.length; i++ ) {
+    //             imgURLs.push(resp[i].urls.regular)
+    //         }
+    //         }
+    //     )
+    //     console.log(respObject.data)
+    //     console.log(respObject)
+    // } catch(err) {
+    //     console.log(err)
+        
+    // }
+    let res = await axios.get(`https://api.unsplash.com/photos/random?count=25&query=abstract&client_id=2xkFYCZvt9fOK2jHkVG4pEayGyEOiwxdtOvPXwfeHPI`)
+
+    unsplashResponse = res
+
+    return res;
+}
+
+function getReqURL(numPhotos) {
+    return "https://api.unsplash.com/photos/random?count=" + numPhotos + "&query=abstract&client_id=2xkFYCZvt9fOK2jHkVG4pEayGyEOiwxdtOvPXwfeHPI"
+}
+
+
 
 app.get('/', async (req,res,next)=> {
     try {
@@ -63,6 +95,16 @@ app.get('/showItem/:name', async (req,res,next)=> {
     }
 })
 
+app.get('/delete/:name', async (req,res,next)=> {
+    try {
+        const deleteItem = await Item.findOneAndDelete({name:req.params.name})
+        res.redirect('/')
+    } catch(err) {
+        console.log(err)
+        next()
+    }
+})
+
 app.get('/seed', async (req,res,next)=> {
     try {
         const newItems = await Item.insertMany(require('./Models/seed.js'))
@@ -74,7 +116,7 @@ app.get('/seed', async (req,res,next)=> {
  }    
 })
 
-app.listen(PORT,(req,res)=> {
+app.listen(PORT, async (req,res)=> {
     console.log(`Listening on port ${PORT}!`)
 })
 
